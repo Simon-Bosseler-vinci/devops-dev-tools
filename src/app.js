cfd7b309c405f33275cdee786d2a117c0a3f9b6a
Diego <dls.tech.teaching@gmail.com>
2023-10-26 16:43:42 +0200

    test: checknames return names without X upper

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 console.log('Hello world');
 
 function checkNames(names) {
-    return names;
+    const updatedNames = [];
+    names.forEach(name => {
+        if (name.indexOf('X') === -1) {
+            updatedNames.push(name)
+        }
+    });
+    return updatedNames;
 }
 module.exports = checkNames;
--- src/app.test.js
+++ src/app.test.js
@@ -1,6 +1,11 @@
 const checkNames = require('./app');
 
-test('should return an empty array', () => {
+test('should return an array with the correct names - all names without x', () => {
   const result = checkNames(['Momo', 'Hadji', 'Leo']);
   expect(result).toEqual(['Momo', 'Hadji', 'Leo']);
+});
+
+test('should return only one name as the rest contains the letter x', () => {
+  const result = checkNames(['DMX', 'Xzibit', 'JeanKevin']);
+  expect(result).toEqual(['JeanKevin']);
 });
