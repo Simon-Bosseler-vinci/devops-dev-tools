c6b2e904569c63088dd18792f66c3bf2f8156b06
Simon <simon.bosseler@student.vinci.be>
2023-12-19 22:23:52 +0100

    add : first test

--- src/app.js
+++ src/app.js
@@ -1,2 +1,6 @@
-console.log('FIXME');
-function sum(a, b, c) { return a + b + c ;} module.exports = sum;
+function sum(a, b) { 
+    return a + b;
+} 
+
+
+module.exports = sum;
--- src/app.test.js
+++ src/app.test.js
@@ -1,5 +1,8 @@
 const sum = require('./app');
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    const result = sum(1,2);
+    expect(result).toBe(3);
 });
+
+
